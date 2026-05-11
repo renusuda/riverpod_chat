09da4546a1c1604191399916b20407cc36f80437
Ren Usuda <rururu.developer@gmail.com>
2026-05-12 07:56:43 +0900

    refactor: textTheme getterを追加

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:riverpod_chat/src/theme/app_spacing.dart';
 
 extension AppTheme on BuildContext {
+  TextTheme get textTheme => Theme.of(this).textTheme;
+
   AppSpacing get spacing => Theme.of(this).extension<AppSpacing>()!;
 }
