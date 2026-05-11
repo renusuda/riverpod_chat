3d8f5302599faf52c82ee2acc4d557758326edcf
Ren Usuda <rururu.developer@gmail.com>
2026-05-12 04:13:42 +0900

    feat: auth featureを追加

---
+++ lib/src/features/auth/pages/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Center(child: Text('login page')),
+    );
+  }
+}
