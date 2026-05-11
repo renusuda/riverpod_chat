38606bb62a4366d83662a798bab806d28d6cbb4e
Ren Usuda <rururu.developer@gmail.com>
2026-05-12 08:07:10 +0900

    fix: ログイン画面のキーボード表示時のはみ出しを修正

--- lib/src/features/auth/pages/login_page.dart
+++ lib/src/features/auth/pages/login_page.dart
@@ -15,28 +15,30 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: spacing.p32),
-          child: Column(
-            children: [
-              Image.asset('assets/app_title.png', width: 300),
-              Text(
-                'すぐにつながる',
-                style: textTheme.titleLarge?.copyWith(
-                  fontWeight: FontWeight.w800,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: spacing.p32),
+            child: Column(
+              children: [
+                Image.asset('assets/app_title.png', width: 300),
+                Text(
+                  'すぐにつながる',
+                  style: textTheme.titleLarge?.copyWith(
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
-              ),
-              Text(
-                '友だちと気軽にチャット',
-                style: textTheme.bodyLarge?.copyWith(color: Colors.grey[600]),
-              ),
-              Image.asset('assets/illustration.png', width: 300),
-              const EmailField(),
-              SizedBox(height: spacing.p16),
-              const PasswordField(),
-              SizedBox(height: spacing.p20),
-              const LoginButton(),
-            ],
+                Text(
+                  '友だちと気軽にチャット',
+                  style: textTheme.bodyLarge?.copyWith(color: Colors.grey[600]),
+                ),
+                Image.asset('assets/illustration.png', width: 300),
+                const EmailField(),
+                SizedBox(height: spacing.p16),
+                const PasswordField(),
+                SizedBox(height: spacing.p20),
+                const LoginButton(),
+              ],
+            ),
           ),
         ),
       ),
