0febfbf70dda8bc18a6e7b3df6a8b84722aa6fa5
Ren Usuda <rururu.developer@gmail.com>
2026-05-11 13:00:05 +0900

    chore: riverpodを使えるようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_chat/src/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(ProviderScope(child: const App()));
 }
