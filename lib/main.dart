f78710feed350556e77bebb12f4460d6689f341a
Ren Usuda <rururu.developer@gmail.com>
2026-05-14 12:52:57 +0900

    feat: Firebase を初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_chat/firebase_options.dart';
 import 'package:riverpod_chat/src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: App()));
 }
