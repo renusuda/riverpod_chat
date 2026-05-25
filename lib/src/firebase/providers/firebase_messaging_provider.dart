6ffbbdefaa7827ce8a5d00d22f8e7261aa96a639
Ren Usuda <rururu.developer@gmail.com>
2026-05-26 01:38:26 +0900

    chore: Firebase Messagingのproviderを追加

---
+++ lib/src/firebase/providers/firebase_messaging_provider.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'firebase_messaging_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+FirebaseMessaging firebaseMessaging(Ref ref) => FirebaseMessaging.instance;
